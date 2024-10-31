64e47ae7ecc37f01e22b28ec44e4d1eb93d99451
Guillermo Robles <grobles64@ieszaidinvergeles.org>
2024-10-31 12:44:56 +0100

    El servidor correspondiente almacena el objeto en MongoDB

--- js_servidor3/index.js
+++ js_servidor3/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import mongoose from "mongoose";
 const app = express();
 const port = 3600;
+const MONGODB_URI = "mongodb://localhost:27017/MensajesServidoresJS";
+mongoose.connect(MONGODB_URI)
+    .then(db => console.log("Conexión establecida con MongoDB"))
+    .catch(err => console.log("Error al conectarse a MongoDB" + err));
 let min = 50;
 let max = 100;
 let numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
@@ -15,8 +20,10 @@ app.post('/', function (req, res) {
         mensaje.sello.push("Sellado por servidor " + mensaje.etapa);
         console.log(mensaje.numero);
     } else {
+        mongoose.connection.collection("objetos").insertOne(mensaje)
+            .then(db => console.log("Objeto guardado en MongoDB"))
+            .catch(err => console.log("Error al guardar el objeto en MongoDB" + err));
         res.send(mensaje);
-
     }
 });
 
